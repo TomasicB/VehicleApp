b189d1db65350964eef66e7b6ccf9828c9300e65
Branimir Tomašić <toma.brane.bt@gmail.com>
2025-08-05 11:24:43 +0200

    first push

---
+++ Vehicle/Models/VehicleEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle.Models;
+
+public class VehicleEngine
+{
+    public string Type { get; set; }
+
+    public string Abrv { get; set; }
+}
---
+++ Vehicle/Models/VehicleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle.Models;
+
+public class VehicleModel
+{
+    public string Name { get; set; }
+
+    public string Abrv { get; set; }
+}
